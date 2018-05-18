e84e74422343fef8af89250a8233215116a41907
Yaroslav Dudchenko <dudchenko_yb@bw-sw.com>
2018-05-18 14:21:01 +0700

    Ticket #4. elastic4s is now used in ElasticsearchKvProcessor implementation instead for elasticsearch-rest-high-level-client

--- build.sbt
+++ build.sbt
@@ -10,15 +10,26 @@ scalaVersion := "2.12.6"
 
 resolvers += Classpaths.typesafeReleases
 
+// major.minor are in sync with the elasticsearch releases
+val elastic4sVersion = "6.2.8"
+
 libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra" % ScalatraVersion,
   "org.scalatra" %% "scalatra-scalatest" % ScalatraVersion % "test",
   "ch.qos.logback" % "logback-classic" % "1.2.3" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "9.4.9.v20180320" % "container",
   "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
-  "org.elasticsearch.client" % "elasticsearch-rest-high-level-client" % "6.2.4",
   "com.typesafe.akka" %% "akka-actor" % "2.5.12",
-  "org.scalamock" %% "scalamock" % "4.1.0" % Test
+  "org.scalamock" %% "scalamock" % "4.1.0" % Test,
+  "com.sksamuel.elastic4s" % "elastic4s-core_2.12" % elastic4sVersion,
+
+  // for the http client
+  "com.sksamuel.elastic4s" % "elastic4s-http_2.12" % elastic4sVersion,
+
+
+  // testing
+  "com.sksamuel.elastic4s" % "elastic4s-testkit_2.12" %  elastic4sVersion % "test",
+  "com.sksamuel.elastic4s" % "elastic4s-tests_2.12" % elastic4sVersion % "test"
 )
 
 enablePlugins(SbtTwirl)
--- src/main/scala/com/bwsw/kv/storage/models/kvprocessor/ElasticsearchKvProcessor.scala
+++ src/main/scala/com/bwsw/kv/storage/models/kvprocessor/ElasticsearchKvProcessor.scala
@@ -1,20 +1,17 @@
 package com.bwsw.kv.storage.models.kvprocessor
 
-import collection.JavaConverters._
-import org.elasticsearch.client.RestHighLevelClient
-import org.elasticsearch.action.ActionListener
-import org.elasticsearch.action.bulk.{BulkRequest, BulkResponse}
-import org.elasticsearch.action.delete.{DeleteRequest, DeleteResponse}
-import org.elasticsearch.action.get._
-import org.elasticsearch.action.index.{IndexRequest, IndexResponse}
-import org.elasticsearch.action.search.{SearchRequest, SearchResponse}
-import org.elasticsearch.index.query.QueryBuilders
-import org.elasticsearch.search.builder.SearchSourceBuilder
+import com.sksamuel.elastic4s.http.{HttpClient, RequestFailure, RequestSuccess}
+import com.sksamuel.elastic4s.http.bulk.BulkResponse
+import com.sksamuel.elastic4s.http.delete.DeleteResponse
+import com.sksamuel.elastic4s.http.get.{GetResponse, MultiGetResponse}
+import com.sksamuel.elastic4s.http.index.IndexResponse
+import com.sksamuel.elastic4s.http.ElasticDsl._
+import com.sksamuel.elastic4s.http.ElasticDsl
 
 import scala.concurrent.{Future, Promise}
 import scala.util.{Failure, Success, Try}
 
-class ElasticsearchKvProcessor(client: RestHighLevelClient) extends KvProcessor{
+class ElasticsearchKvProcessor(client: HttpClient) extends KvProcessor{
   import scala.concurrent.ExecutionContext.Implicits.global
 
   /** Gets value by given key
@@ -23,23 +20,21 @@ class ElasticsearchKvProcessor(client: RestHighLevelClient) extends KvProcessor{
     * @return the value
     */
   def get(storage: String, key: String): Future[String] = {
-    val prom = Promise[String]()
-    val getListener: ActionListener[GetResponse] =  new ActionListener[GetResponse]() {
-      @Override
-      def onResponse(getResponse: GetResponse) {
-        if (getResponse.isExists)
-          prom.complete(Try(getResponse.getField("value").getValue))
-        else
-          prom.failure(new NullPointerException)
-      }
-      @Override
-      def onFailure(e: Exception) {
-        prom.failure(e)
-      }
-    }
-    val getRequest = new GetRequest(storage, "_doc", key)
-    client.getAsync(getRequest, getListener)
-    prom.future
+    client.execute {
+      ElasticDsl.get(key).from(storage / "_doc")
+    }.transformWith({
+      case Success(either: Either[RequestFailure, RequestSuccess[GetResponse]]) =>
+        either match {
+          case Left(failure) => Future { throw new Exception(failure.body.getOrElse("Request failed")) }
+          case Right(success) => Future {
+            if (success.isError)
+              throw new Exception("Request failed")
+            else
+              success.result.fields("value").toString
+          }
+        }
+      case Failure(either) => Future { throw either.getCause}
+    })
   }
 
   /** Gets value by given keys
@@ -48,28 +43,25 @@ class ElasticsearchKvProcessor(client: RestHighLevelClient) extends KvProcessor{
     * @return Collection of tuples key and Some(String) if value exists and None otherwise
     */
   def get(storage: String, keys: Iterable[String]): Future[Iterable[(String, Option[String])]] = {
-    val prom = Promise[Iterable[(String, Option[String])]]()
-    val multiGetListener: ActionListener[MultiGetResponse] =  new ActionListener[MultiGetResponse]() {
-      @Override
-      def onResponse(multiGetResponse: MultiGetResponse) {
-        prom.complete(Try(multiGetResponse.getResponses.map{ response =>
-          if(response.isFailed) {
-            (response.getResponse().getId, None)
+    val gets = keys.map{ ElasticDsl.get(_).from(storage / "_doc") }
+    client.execute { multiget(gets) }
+      .transformWith({
+      case Success(either: Either[RequestFailure, RequestSuccess[MultiGetResponse]]) =>
+        either match {
+          case Left(failure) => Future { throw new Exception(failure.body.getOrElse("Request failed")) }
+          case Right(success) => Future {
+            if (success.isError)
+              throw new Exception("Request failed")
+            else
+              success.result.docs.map( getResponse =>
+                if(getResponse.found)
+                  (getResponse.id, Some(getResponse.fields("value").toString))
+                else
+                  (getResponse.id, None) )
           }
-          else {
-            val aGetResponse = response.getResponse()
-            (aGetResponse.getId, Some(aGetResponse.getField("value").getValue))
-          } }))
-      }
-      @Override
-      def onFailure(e: Exception) {
-        prom.failure(e)
-      }
-    }
-    val multiGetRequest = new MultiGetRequest()
-    keys.foreach(key => multiGetRequest.add(storage, "_doc", key))
-    client.multiGetAsync(multiGetRequest, multiGetListener)
-    prom.future
+        }
+      case Failure(either) => Future { throw either.getCause}
+    })
   }
 
   /** Sets value for given key
@@ -79,21 +71,22 @@ class ElasticsearchKvProcessor(client: RestHighLevelClient) extends KvProcessor{
     * @return true
     */
   def set(storage: String, key: String, value: String): Future[Boolean] = {
-    val prom = Promise[Boolean]()
-    val indexListener: ActionListener[IndexResponse] =  new ActionListener[IndexResponse]() {
-      @Override
-      def onResponse(indexResponse: IndexResponse) {
-        prom.complete(Try(true))
-      }
-      @Override
-      def onFailure(e: Exception) {
-        prom.failure(e)
-      }
-    }
-    val indexRequest = new IndexRequest(storage, "_doc", key)
-      .source("value", value)
-    client.indexAsync(indexRequest, indexListener)
-    prom.future
+    client.execute {
+      indexInto(storage / "_doc") id key fields (
+        "value" -> value )
+    }.transformWith({
+      case Success(either: Either[RequestFailure, RequestSuccess[IndexResponse]]) =>
+        either match {
+          case Left(failure) => Future { throw new Exception(failure.body.getOrElse("Request failed")) }
+          case Right(success) => Future {
+            if (success.isError)
+              throw new Exception("Request failed")
+            else
+              true
+          }
+        }
+      case Failure(either) => Future { throw either.getCause}
+    })
   }
 
   /** Sets values by given keys
@@ -102,23 +95,25 @@ class ElasticsearchKvProcessor(client: RestHighLevelClient) extends KvProcessor{
     * @return Collection of tuples key to true if value successfully set and false otherwise
     */
   def set(storage: String, kvs: Iterable[(String, String)]): Future[Iterable[(String, Boolean)]] = {
-    val prom = Promise[Iterable[(String, Boolean)]]()
-    val bulkListener: ActionListener[BulkResponse] =  new ActionListener[BulkResponse]() {
-      @Override
-      def onResponse(bulkResponse: BulkResponse) {
-        prom.complete(Try(bulkResponse.getItems.map{ response =>
-          val indexResponse: IndexResponse = response.getResponse()
-          (indexResponse.getId, !response.isFailed) }))
-      }
-      @Override
-      def onFailure(e: Exception) {
-        prom.failure(e)
-      }
-    }
-    val bulkRequest = new BulkRequest
-    kvs.foreach{ case (key, value) => bulkRequest.add(new IndexRequest(storage, "_doc", key).source("value", value)) }
-    client.bulkAsync(bulkRequest, bulkListener)
-    prom.future
+    import com.sksamuel.elastic4s.http.ElasticDsl._
+
+    val sets = kvs.map{ case (key, value) => indexInto(storage / "_doc") id key fields (
+      "value" -> value ) }
+    client.execute { bulk(sets) }
+      .transformWith({
+        case Success(either: Either[RequestFailure, RequestSuccess[BulkResponse]]) =>
+          either match {
+            case Left(failure) => Future { throw new Exception(failure.body.getOrElse("Request failed")) }
+            case Right(success) => Future {
+              if (success.isError)
+                throw new Exception("Request failed")
+              else
+                success.result.items.map( bulkResponseItem =>
+                  (bulkResponseItem.id, bulkResponseItem.error.isEmpty) )
+            }
+          }
+        case Failure(either) => Future { throw either.getCause}
+      })
   }
 
   /** Deletes value of given key
@@ -127,20 +122,23 @@ class ElasticsearchKvProcessor(client: RestHighLevelClient) extends KvProcessor{
     * @return true
     */
   def delete(storage: String, key: String): Future[Boolean] = {
-    val prom = Promise[Boolean]()
-    val deleteListener: ActionListener[DeleteResponse] =  new ActionListener[DeleteResponse]() {
-      @Override
-      def onResponse(deleteResponse: DeleteResponse) {
-        prom.complete(Try(true))
-      }
-      @Override
-      def onFailure(e: Exception) {
-        prom.failure(e)
-      }
-    }
-    val deleteRequest = new DeleteRequest(storage, "_doc", key)
-    client.deleteAsync(deleteRequest, deleteListener)
-    prom.future
+    import com.sksamuel.elastic4s.http.ElasticDsl._
+
+    client.execute {
+      deleteById(storage,"_doc", key)
+    }.transformWith({
+      case Success(either: Either[RequestFailure, RequestSuccess[DeleteResponse]]) =>
+        either match {
+          case Left(failure) => Future { throw new Exception(failure.body.getOrElse("Request failed")) }
+          case Right(success) => Future {
+            if (success.isError)
+              throw new Exception("Request failed")
+            else
+              true
+          }
+        }
+      case Failure(either) => Future { throw either.getCause}
+    })
   }
 
   /** Deletes values of given keys
@@ -149,23 +147,24 @@ class ElasticsearchKvProcessor(client: RestHighLevelClient) extends KvProcessor{
     * @return Collection of tuples key to true if deletion succeed or false otherwise
     */
   def delete(storage: String, keys: Iterable[String]): Future[Iterable[(String, Boolean)]] = {
-    val prom = Promise[Iterable[(String, Boolean)]]()
-    val bulkListener: ActionListener[BulkResponse] =  new ActionListener[BulkResponse]() {
-      @Override
-      def onResponse(bulkResponse: BulkResponse) {
-        prom.complete(Try(bulkResponse.getItems.map{ response =>
-          val deleteResponse: DeleteResponse = response.getResponse()
-          (deleteResponse.getId, !response.isFailed) }))
-      }
-      @Override
-      def onFailure(e: Exception) {
-        prom.failure(e)
-      }
-    }
-    val bulkRequest = new BulkRequest
-    keys.foreach{ key => bulkRequest.add(new DeleteRequest(storage, "_doc", key)) }
-    client.bulkAsync(bulkRequest, bulkListener)
-    prom.future
+    import com.sksamuel.elastic4s.http.ElasticDsl._
+
+    val deletes = keys.map{ deleteById(storage,"_doc", _) }
+    client.execute { bulk(deletes) }
+      .transformWith({
+        case Success(either: Either[RequestFailure, RequestSuccess[BulkResponse]]) =>
+          either match {
+            case Left(failure) => Future { throw new Exception(failure.body.getOrElse("Request failed")) }
+            case Right(success) => Future {
+              if (success.isError)
+                throw new Exception("Request failed")
+              else
+                success.result.items.map( bulkResponseItem =>
+                  (bulkResponseItem.id, bulkResponseItem.error.isEmpty) )
+            }
+          }
+        case Failure(either) => Future { throw either.getCause}
+      })
   }
 
   /** Returns a list of existing keys and values
