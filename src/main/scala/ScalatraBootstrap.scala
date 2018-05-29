b948dc2cdbf74ae59e7105b349297501c49914d4
Ksenia Vazhdaeva <vazhdaeva_ks@bitworks-software.com>
2018-05-29 09:49:39 +0700

    Ticket #6. Fix code style for class variables.

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -4,20 +4,20 @@ import com.bwsw.kv.storage.app.Configuration
 import com.bwsw.kv.storage.processor.ElasticsearchKvProcessor
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.HttpClient
-import org.scalatra._
 import javax.servlet.ServletContext
+import org.scalatra._
 
 class ScalatraBootstrap extends LifeCycle {
-  val Conf = new Configuration
-  val Client = HttpClient(ElasticsearchClientUri(Conf.getElasticsearchUri))
-  val Processor = new ElasticsearchKvProcessor(Client, Conf)
-  val System = ActorSystem()
+  val conf = new Configuration
+  val client = HttpClient(ElasticsearchClientUri(conf.getElasticsearchUri))
+  val processor = new ElasticsearchKvProcessor(client, conf)
+  val system = ActorSystem()
 
   override def init(context: ServletContext) {
-    context.mount(new KvStorageServlet(System, Processor), "/*")
+    context.mount(new KvStorageServlet(system, processor), "/*")
   }
 
-  override def destroy(context:ServletContext) {
-    System.terminate
+  override def destroy(context: ServletContext) {
+    system.terminate
   }
 }
