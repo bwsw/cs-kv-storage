e2c235aa820bc7df5f01a7ade116d8f37e5ca020
Ksenia Vazhdaeva <vazhdaeva_ks@bitworks-software.com>
2018-06-26 15:32:52 +0700

    Ticket #25. Add license headers.

--- src/test/scala/com/bwsw/cloudstack/storage/kv/actor/BufferedHistoryKvActorSpec.scala
+++ src/test/scala/com/bwsw/cloudstack/storage/kv/actor/BufferedHistoryKvActorSpec.scala
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package com.bwsw.cloudstack.storage.kv.actor
 
 import akka.actor.{ActorSystem, Props}
@@ -5,22 +22,22 @@ import akka.testkit.{ImplicitSender, TestKit, _}
 import com.bwsw.cloudstack.storage.kv.cache.StorageCache
 import com.bwsw.cloudstack.storage.kv.configuration.AppConfig
 import com.bwsw.cloudstack.storage.kv.entity.Storage
-import com.bwsw.cloudstack.storage.kv.message.{Delete, Set, Clear, KvHistory, KvHistoryBulk}
+import com.bwsw.cloudstack.storage.kv.message.{Clear, Delete, KvHistory, KvHistoryBulk, Set}
 import com.bwsw.cloudstack.storage.kv.processor.HistoryProcessor
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, FunSpecLike}
 import scaldi.{Injector, Module}
 
-import scala.concurrent.duration._
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 class BufferedHistoryKvActorSpec
   extends TestKit(ActorSystem("cs-kv-storage"))
-    with FunSpecLike
-    with MockFactory
-    with ImplicitSender
-    with BeforeAndAfterAll
-    with BeforeAndAfterEach {
+  with FunSpecLike
+  with MockFactory
+  with ImplicitSender
+  with BeforeAndAfterAll
+  with BeforeAndAfterEach {
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
