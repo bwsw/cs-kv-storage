6722ea9df234caab605a3d351a7194e0a2eaa792
Yaroslav Dudchenko <dudchenko_yb@bw-sw.com>
2018-07-02 16:54:41 +0700

    Ticket #18. WIP: Tests for value management added

--- e2e/initializer/etc/cs-kv-storage/init.sh
+++ e2e/initializer/etc/cs-kv-storage/init.sh
@@ -3,8 +3,19 @@
 curl -X PUT "$1/storage-registry" -H 'Content-Type: application/json' --data-binary \
   "@/etc/cs-kv-storage/storage-registry.json"
 
-curl -X PUT "$1/_template/storage-template" -H 'Content-Type: application/json' --data-binary \
+curl -X PUT "$1/_template/storage" -H 'Content-Type: application/json' --data-binary \
   "@/etc/cs-kv-storage/storage-template.json"
 
-curl -X PUT "$1/storage-test"
+curl -X PUT "$1/_template/history-storage" -H 'Content-Type: application/json' --data-binary \
+  "@/etc/cs-kv-storage/storage-history-template.json"
+
+curl -X PUT "$1/storage-read-only"
+
+curl -X PUT "$1/storage-registry/_doc/storage-read-only" -H 'Content-Type: application/json' --data-raw \
+  '{"type": "ACC", "is_history_enabled": true}'
+
+curl -X PUT "$1/storage-read-only/_doc/_bulk" -H 'Content-Type: application/json' --data-binary \
+  "@/etc/cs-kv-storage/init/storage-read-only.ndjson"
+
+curl -X PUT "$1/storage-empty"
 
