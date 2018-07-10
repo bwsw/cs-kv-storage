d12d5f0a8d4654979a9ed9da94cf8cd5d98b10c3
Yaroslav Dudchenko <dudchenko_yb@bw-sw.com>
2018-07-10 09:37:48 +0700

    Ticket #29. Useless formatting finally rolled back.

--- src/main/scala/com/bwsw/cloudstack/storage/kv/app/KvStorageModule.scala
+++ src/main/scala/com/bwsw/cloudstack/storage/kv/app/KvStorageModule.scala
@@ -3,16 +3,12 @@ package com.bwsw.cloudstack.storage.kv.app
 import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, Materializer}
 import com.bwsw.cloudstack.storage.kv.actor._
-import com.bwsw.cloudstack.storage.kv.cache.{
-  ElasticsearchStorageLoader, LoadingStorageCache, StorageCache,
-  StorageLoader
-}
+import com.bwsw.cloudstack.storage.kv.cache.{ElasticsearchStorageLoader, LoadingStorageCache, StorageCache,
+  StorageLoader}
 import com.bwsw.cloudstack.storage.kv.configuration.{AppConfig, ElasticsearchConfig}
 import com.bwsw.cloudstack.storage.kv.manager.{ElasticsearchKvStorageManager, KvStorageManager}
-import com.bwsw.cloudstack.storage.kv.processor.{
-  ElasticsearchHistoryProcessor, ElasticsearchKvProcessor,
-  HistoryProcessor, KvProcessor
-}
+import com.bwsw.cloudstack.storage.kv.processor.{ElasticsearchHistoryProcessor, ElasticsearchKvProcessor,
+  HistoryProcessor, KvProcessor}
 import com.bwsw.cloudstack.storage.kv.util.Clock
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.HttpClient
