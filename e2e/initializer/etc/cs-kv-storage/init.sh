f3e38edf16950702483db60498be37bc66503e7c
Ksenia Vazhdaeva <vazhdaeva_ks@bitworks-software.com>
2018-07-12 15:27:37 +0700

    Ticket #18. Start e2e tests after data preparation.

--- e2e/initializer/etc/cs-kv-storage/init.sh
+++ e2e/initializer/etc/cs-kv-storage/init.sh
@@ -1,84 +1,87 @@
 #!/bin/bash
 
-curl -X PUT "$1/storage-registry" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/storage-registry.json"
+put_binary() {
+    curl -s -o /dev/null -S --fail -X PUT "$1" -H 'Content-Type: application/json' --data-binary "$2" || exit 1
+}
 
-curl -X PUT "$1/_template/storage-data" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/storage-data-template.json"
+put() {
+    if [ -z "$2" ]
+    then
+        curl -s -o /dev/null -S --fail -X PUT "$1" || exit 1
+    else
+        curl -s -o /dev/null -S --fail -X PUT "$1" -H 'Content-Type: application/json' --data-raw "$2" || exit 1
+    fi
+}
 
-curl -X PUT "$1/_template/storage-history" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/storage-history-template.json"
+post_binary() {
+    curl -s -o /dev/null -S --fail -X POST "$1" -H 'Content-Type: application/json' --data-binary "$2" || exit 1
+}
 
-curl -X PUT "$1/storage-data-read-only"
+echo "Test data preparation started"
 
-curl -X PUT "$1/storage-registry/_doc/read-only" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "ACC", "history_enabled": true}'
+put_binary "$1/storage-registry" "@/etc/cs-kv-storage/storage-registry.json"
 
-curl -X POST "$1/storage-data-read-only/_doc/_bulk" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
+put_binary "$1/_template/storage-data" "@/etc/cs-kv-storage/storage-data-template.json"
 
-curl -X POST "$1/storage-history-read-only/_doc/_bulk" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/init/storage-test-history.ndjson"
+put_binary "$1/_template/storage-history" "@/etc/cs-kv-storage/storage-history-template.json"
 
-curl -X PUT "$1/storage-data-empty"
+put "$1/storage-data-read-only"
 
-curl -X PUT "$1/storage-registry/_doc/empty" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "ACC", "history_enabled": true}'
-s
-curl -X PUT "$1/storage-data-editable-single"
+put "$1/storage-registry/_doc/read-only" '{"type": "ACC", "history_enabled": true}'
 
-curl -X PUT "$1/storage-registry/_doc/editable-single" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "ACC", "history_enabled": true}'
+post_binary "$1/storage-data-read-only/_doc/_bulk" "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
 
-curl -X POST "$1/storage-data-editable-single/_doc/_bulk" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
+post_binary "$1/storage-history-read-only/_doc/_bulk" "@/etc/cs-kv-storage/init/storage-test-history.ndjson"
 
-curl -X PUT "$1/storage-data-editable-multiple"
+put "$1/storage-data-empty"
 
-curl -X PUT "$1/storage-registry/_doc/editable-multiple" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/empty" '{"type": "ACC", "history_enabled": true}'
 
-curl -X POST "$1/storage-data-editable-multiple/_doc/_bulk" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
+put "$1/storage-data-editable-single"
 
-curl -X PUT "$1/storage-data-deletable-single"
+put "$1/storage-registry/_doc/editable-single" '{"type": "ACC", "history_enabled": true}'
 
-curl -X PUT "$1/storage-registry/_doc/deletable-single" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "ACC", "history_enabled": true}'
+post_binary "$1/storage-data-editable-single/_doc/_bulk" "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
 
-curl -X POST "$1/storage-data-deletable-single/_doc/_bulk" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
+put "$1/storage-data-editable-multiple"
 
-curl -X PUT "$1/storage-data-deletable-multiple"
+put "$1/storage-registry/_doc/editable-multiple" '{"type": "ACC", "history_enabled": true}'
 
-curl -X PUT "$1/storage-registry/_doc/deletable-multiple" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "ACC", "history_enabled": true}'
+post_binary "$1/storage-data-editable-multiple/_doc/_bulk" "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
 
-curl -X POST "$1/storage-data-deletable-multiple/_doc/_bulk" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
+put "$1/storage-data-deletable-single"
 
-curl -X PUT "$1/storage-data-cleanable"
+put "$1/storage-registry/_doc/deletable-single" '{"type": "ACC", "history_enabled": true}'
 
-curl -X PUT "$1/storage-registry/_doc/cleanable" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "ACC", "history_enabled": true}'
+put_binary "$1/storage-data-deletable-single/_doc/_bulk" "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
 
-curl -X POST "$1/storage-data-cleanable/_doc/_bulk" -H 'Content-Type: application/json' --data-binary \
-  "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
+put "$1/storage-data-deletable-multiple"
 
-curl -X PUT "$1/storage-data-temp"
+put "$1/storage-registry/_doc/deletable-multiple" '{"type": "ACC", "history_enabled": true}'
 
-curl -X PUT "$1/storage-registry/_doc/temp" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1010000}'
+put_binary "$1/storage-data-deletable-multiple/_doc/_bulk" "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
 
-curl -X PUT "$1/storage-data-account"
+put "$1/storage-data-cleanable"
 
-curl -X PUT "$1/storage-registry/_doc/account" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/cleanable" '{"type": "ACC", "history_enabled": true}'
 
-curl -X PUT "$1/storage-data-no-index"
+post_binary "$1/storage-data-cleanable/_doc/_bulk" "@/etc/cs-kv-storage/init/storage-test-data.ndjson"
 
-curl -X PUT "$1/storage-registry/_doc/not-registered" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1530602449565}'
+put "$1/storage-data-temp"
 
-curl -X PUT "$1/storage-registry/_doc/no-history" -H 'Content-Type: application/json' --data-raw \
-  '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1530602449565}'
+put "$1/storage-registry/_doc/temp" \
+    '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1010000}'
+
+put "$1/storage-data-account"
+
+put "$1/storage-registry/_doc/account" '{"type": "ACC", "history_enabled": true}'
+
+put "$1/storage-data-no-index"
+
+put "$1/storage-registry/_doc/not-registered" \
+    '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1530602449565}'
+
+put "$1/storage-registry/_doc/no-history" \
+    '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1530602449565}'
+
+echo "Test data preparation finished"
