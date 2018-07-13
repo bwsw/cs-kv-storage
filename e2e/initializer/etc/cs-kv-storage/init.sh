f4d9714f28ffdceb8efa49c5c06465c5033d2aa7
Ksenia Vazhdaeva <vazhdaeva_ks@bitworks-software.com>
2018-07-13 10:08:38 +0700

    Ticket #31. Fix tests for storage deletion.

--- e2e/initializer/etc/cs-kv-storage/init.sh
+++ e2e/initializer/etc/cs-kv-storage/init.sh
@@ -27,7 +27,7 @@ put_binary "$1/_template/storage-history" "@/etc/cs-kv-storage/configuration/sto
 
 put "$1/storage-data-read-only"
 
-put "$1/storage-registry/_doc/read-only" '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/read-only" '{"type": "ACCOUNT", "deleted": false, "history_enabled": true}'
 
 post_binary "$1/storage-data-read-only/_doc/_bulk" "@/etc/cs-kv-storage/data/storage-test-data.ndjson"
 
@@ -35,53 +35,50 @@ post_binary "$1/storage-history-read-only/_doc/_bulk" "@/etc/cs-kv-storage/data/
 
 put "$1/storage-data-empty"
 
-put "$1/storage-registry/_doc/empty" '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/empty" '{"type": "ACCOUNT", "deleted": false, "history_enabled": true}'
 
 put "$1/storage-data-editable-single"
 
-put "$1/storage-registry/_doc/editable-single" '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/editable-single" '{"type": "ACCOUNT", "deleted": false, "history_enabled": true}'
 
 post_binary "$1/storage-data-editable-single/_doc/_bulk" "@/etc/cs-kv-storage/data/storage-test-data.ndjson"
 
 put "$1/storage-data-editable-multiple"
 
-put "$1/storage-registry/_doc/editable-multiple" '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/editable-multiple" '{"type": "ACCOUNT", "deleted": false, "history_enabled": true}'
 
 post_binary "$1/storage-data-editable-multiple/_doc/_bulk" "@/etc/cs-kv-storage/data/storage-test-data.ndjson"
 
 put "$1/storage-data-deletable-single"
 
-put "$1/storage-registry/_doc/deletable-single" '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/deletable-single" '{"type": "ACCOUNT", "deleted": false, "history_enabled": true}'
 
 put_binary "$1/storage-data-deletable-single/_doc/_bulk" "@/etc/cs-kv-storage/data/storage-test-data.ndjson"
 
 put "$1/storage-data-deletable-multiple"
 
-put "$1/storage-registry/_doc/deletable-multiple" '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/deletable-multiple" '{"type": "ACCOUNT", "deleted": false, "history_enabled": true}'
 
 put_binary "$1/storage-data-deletable-multiple/_doc/_bulk" "@/etc/cs-kv-storage/data/storage-test-data.ndjson"
 
 put "$1/storage-data-cleanable"
 
-put "$1/storage-registry/_doc/cleanable" '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/cleanable" '{"type": "ACCOUNT", "deleted": false, "history_enabled": true}'
 
 post_binary "$1/storage-data-cleanable/_doc/_bulk" "@/etc/cs-kv-storage/data/storage-test-data.ndjson"
 
 put "$1/storage-data-temp"
 
 put "$1/storage-registry/_doc/temp" \
-    '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1010000}'
+    '{"type": "TEMP", "deleted": false, "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1010000}'
 
 put "$1/storage-data-account"
 
-put "$1/storage-registry/_doc/account" '{"type": "ACC", "history_enabled": true}'
+put "$1/storage-registry/_doc/account" '{"type": "ACCOUNT", "deleted": false, "history_enabled": true}'
 
 put "$1/storage-data-no-index"
 
-put "$1/storage-registry/_doc/not-registered" \
-    '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1530602449565}'
-
 put "$1/storage-registry/_doc/no-history" \
-    '{"type": "TEMP", "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1530602449565}'
+    '{"type": "TEMP", "deleted": false, "history_enabled": false, "ttl": 10000, "expiration_timestamp": 1530602449565}'
 
 echo "Test data preparation finished"
